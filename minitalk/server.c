8a56a8a8ea850a143f914399630f16b8b075e9ac
GmihSoukaina <soukaina4125@gmail.com>
2025-02-14 12:50:02 +0100

    commit

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: sgmih <sgmih@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 09:18:31 by sgmih             #+#    #+#             */
-/*   Updated: 2025/02/14 10:40:51 by sgmih            ###   ########.fr       */
+/*   Updated: 2025/02/14 12:49:09 by sgmih            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,64 +30,65 @@ int tag(void) {
     return 0;
 }
 
-static unsigned char current_char = 0; // Variable to store the current character
-static int bit_count = 0; // Counter to track the number of bits received
+void	reset(pid_t *pid, unsigned char *current_char, int *bit_count)
+{
+	*pid = 0;
+	*current_char = 0;
+	*bit_count = 0;
+}
+
+static void	handle_bit(int signum, unsigned char *current_char, int *bit_count)
+{
+	if (signum == SIGUSR2)
+		*current_char |= 1 << (7 - *bit_count);
+	else
+		*current_char |= (0 << (7 - *bit_count));
+	(*bit_count)++;
+}
+
+static void process_char(unsigned char *current_char, int *bit_count)
+{
+    if (*bit_count == 8)
+    {
+        if (*current_char == '\0')
+        {
+            write(1, "\n", 1);
+            *bit_count = 0;
+            *current_char = 0;
+            return ;
+        }
+        write(1, current_char, 1);
+        *current_char = 0;
+        *bit_count = 0;
+    }
+}
 
 void signal_handler(int signum, siginfo_t *info, void *context)
 {
     (void)context;
-    static pid_t pid = 0;
+    static pid_t pid;
+	static unsigned char current_char ;
+	static int bit_count ;
 
+	pid = 0;
+	current_char = 0;
+	bit_count = 0;
     if (pid == 0)
         pid = info->si_pid;
     if (info->si_pid != pid)
-    {
-        pid = info->si_pid;
-        bit_count = 0;
-        current_char = 0;
-    }
-
-    if (signum == SIGUSR2)
-        current_char |= 1 << (7 - bit_count);  // Set the bit if SIGUSR2
-    else
-    current_char |= (0 << (7 - bit_count)); 
-
-    // Increment the bit counter
-    bit_count++;
-
-    // If 8 bits have been received, process the character
-    if (bit_count == 8)
-    {
-        // Check if the received character is the null character
-        if (current_char == '\0') //?
-        {
-            // Print a newline to indicate the end of the message
-            write(1, "\n", 1);
-            // Reset for the next message
-            bit_count = 0;
-            current_char = 0;
-            return;
-        }
-        write(1, &current_char, 1);
-        current_char = 0;
-        bit_count = 0;
-    }
+		reset(&pid, &current_char, &bit_count);
+	handle_bit(signum, &current_char, &bit_count);
+    process_char(&current_char, &bit_count);
 }
 
 
 int main(void)
 {
     struct sigaction sig;
-    //printf("Server PID: %d\n", getpid());
-
     tag();
     ft_printf("\n   Server PID  →  \t%d\n", getpid());
-
-    // Set up the sigaction structure
 	sig.sa_flags = SA_SIGINFO;
 	sig.sa_sigaction = signal_handler;
-
-    // Register the signal handler for SIGUSR1 and SIGUSR2
 	sigaction(SIGUSR1, &sig, 0);
 	sigaction(SIGUSR2, &sig, 0);
 	while (1)
